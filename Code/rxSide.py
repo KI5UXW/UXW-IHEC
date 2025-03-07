5f30257d3482bb91c21827fce3b930152d9d3333
KI5UXW <ki5uxw@tamu.edu>
2025-03-07 09:55:51 -0600

    Figured out the time sync thing on windows.

--- Code/rxSide.py
+++ Code/rxSide.py
@@ -1,2 +1,5 @@
 #KI5UXW's Ionospheric Height Experiment Contribution
+# Receive Side.
 
+# If on Windows, make sure that automatic time sync is on. 
+# Handy guide here: https://www.elevenforum.com/t/windows-time-is-not-syncing-automatically.17871/
---
+++ Code/txSide.py
@@ -0,0 +1,6 @@
+#KI5UXW's Ionospheric Height Experiment Contribution
+# Transmit Side.
+
+# If on Windows, make sure that automatic time sync is on. 
+# Handy guide here: https://www.elevenforum.com/t/windows-time-is-not-syncing-automatically.17871/
+
