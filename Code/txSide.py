eb33011dcce51303faddc76464ec41265c279348
KI5UXW <ki5uxw@tamu.edu>
2025-03-10 13:04:14 -0500

    Prototype of sender in progress.

--- Code/txSide.py
+++ Code/txSide.py
@@ -4,8 +4,25 @@
 # If on Windows, make sure that automatic time sync is on. 
 # Handy guide here: https://www.elevenforum.com/t/windows-time-is-not-syncing-automatically.17871/
 
+# CW Start Message Reads: MSG DE KI5UXW. Ionospheric sounding experiment about to start. Please clear frequency if possible. Experiment repeats every 15 minutes of the hour. Contact KI5UXW, K1FR, or WA5FRF for information.
+# CW End Message Reads: MSG DE KI5UXW. Ionospheric sounding experiment completed. Experiment repeats every 15 minutes of the hour. Contact KI5UXW, K1FR, or WA5FRF for information.
+
 import winsound
+from datetime import datetime
+import time
+
 file = "C:/Users/ervin/UXW-IHEC/AudioFiles/SEQP Test Signal_v5_Science-payload.wav"
-#os.system("afplay " + file)
-#os.system("mpg123 " + file)
-winsound.PlaySound(file, winsound.SND_FILENAME)
+messageCWstart = "C:/Users/ervin/UXW-IHEC/AudioFiles/startCW.wav"
+messageCWend = "C:/Users/ervin/UXW-IHEC/AudioFiles/endCW.wav"
+
+def mark1():
+    timeList = [0, 15, 30, 45]
+    while 0 == 0:
+        myobj = datetime.now()
+        while myobj.minute not in timeList:
+            myobj = datetime.now()
+            time.sleep(1)
+            print(myobj)
+        winsound.PlaySound(messageCWstart, winsound.SND_FILENAME)
+        winsound.PlaySound(file, winsound.SND_FILENAME)
+        winsound.PlaySound(messageCWend, winsound.SND_FILENAME)
