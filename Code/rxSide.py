862e60f3ee9b17c8352c31238c08c8ccae5a0b53
KI5UXW <ki5uxw@tamu.edu>
2025-03-10 11:52:48 -0500

    Finally got Windows to play audio.

--- Code/rxSide.py
+++ Code/rxSide.py
@@ -3,3 +3,10 @@
 
 # If on Windows, make sure that automatic time sync is on. 
 # Handy guide here: https://www.elevenforum.com/t/windows-time-is-not-syncing-automatically.17871/
+
+def runRX(timesPerHour):
+    timeRX = timesPerHour / 60
+    if timeRX < 3:
+        print('ERROR: Transmits too often. Please try again.')
+        return
+    pass
--- Code/txSide.py
+++ Code/txSide.py
@@ -4,3 +4,8 @@
 # If on Windows, make sure that automatic time sync is on. 
 # Handy guide here: https://www.elevenforum.com/t/windows-time-is-not-syncing-automatically.17871/
 
+import winsound
+file = "C:/Users/ervin/UXW-IHEC/AudioFiles/SEQP Test Signal_v5_Science-payload.wav"
+#os.system("afplay " + file)
+#os.system("mpg123 " + file)
+winsound.PlaySound(file, winsound.SND_FILENAME)
