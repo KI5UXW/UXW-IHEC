c886ecb48f730d7a65d29d86959c1ab0de89bb29
KI5UXW <ervinhfamily@gmail.com>
2025-04-10 16:30:43 -0500

    Fixed issue with audio transmission.

--- Code/Mark1/txSideMark1.py
+++ Code/Mark1/txSideMark1.py
@@ -7,13 +7,14 @@
 # CW Start Message Reads: MSG DE KI5UXW. Ionospheric sounding experiment about to start. Please clear frequency if possible. Experiment repeats every 15 minutes of the hour. Contact KI5UXW, K1FR, or WA5FRF for information.
 # CW End Message Reads: MSG DE KI5UXW. Ionospheric sounding experiment completed. Experiment repeats every 15 minutes of the hour. Contact KI5UXW, K1FR, or WA5FRF for information.
 
-import winsound
+import sounddevice as sd
+import soundfile as sf
 from datetime import datetime
 import time
 
-file = "C:/Users/ervin/UXW-IHEC/AudioFiles/SEQP Test Signal_v5_Science-payload.wav"
-messageCWstart = "C:/Users/ervin/UXW-IHEC/AudioFiles/shortStart.wav"
-messageCWend = "C:/Users/ervin/UXW-IHEC/AudioFiles/shortEnd.wav"
+payloadTrack = 'AudioFiles/SEQP Test Signal_v5_Science-payload.wav'
+cwPre = "AudioFiles/shortStart.wav"
+cwPost = "AudioFiles/shortEnd.wav"
 
 def mark1TX():
     print("KI5UXW's Ionospheric Height Experiment Contribution.")
@@ -26,12 +27,19 @@ def mark1TX():
             myobj = datetime.now()
             time.sleep(1)
             print(myobj)
-        winsound.PlaySound(messageCWstart, winsound.SND_FILENAME) #0:19
-        winsound.PlaySound(file, winsound.SND_FILENAME) #0:18
-        winsound.PlaySound(messageCWend, winsound.SND_FILENAME) #0:19
+        data, fs = sf.read(cwPre, dtype='float32')  
+        sd.play(data, fs)
+        status = sd.wait()
+        data, fs = sf.read(payloadTrack, dtype='float32')  
+        sd.play(data, fs)
+        status = sd.wait()
+        data, fs = sf.read(cwPost, dtype='float32')  
+        sd.play(data, fs)
+        status = sd.wait()  # Wait until file is done playing
         # Total duration is 56 seconds.
 
 myobj = datetime.now()
+print('Running sync with computer clock...')
 while myobj.second != 0:
     myobj = datetime.now()
     #print(myobj.second)
